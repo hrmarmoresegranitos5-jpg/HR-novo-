03afd8b3fa9572d5a8bb3293c0fadcd1a4c4b516
hrmarmoresegranitos5-jpg <hrmarmoresegranitos5@gmail.com>
2026-05-10 16:15:27 -0300

    Update via GitHub Deployer — 10/05/2026, 16:15:26

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // ═══════════════════════════════════════════════════════
-// HR Mármores e Granitos — Service Worker v21
+// HR Mármores e Granitos — Service Worker v22
 // ═══════════════════════════════════════════════════════
 
-var CACHE_VERSION = 'hr-app-v21';
+var CACHE_VERSION = 'hr-app-v22';
 
 var APP_SHELL = [
   '/Novo-app/index.html',
